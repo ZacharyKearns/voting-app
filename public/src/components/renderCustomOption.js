79c5b96b9d4030db61e6bcc5016ca3fb8d198c2d
Zachary Kearns <zach_kearns89@live.com>
2017-02-15 18:52:43 -0600

    change input width

--- public/src/components/renderCustomOption.js
+++ public/src/components/renderCustomOption.js
@@ -3,7 +3,7 @@ import React from 'react';
 const renderCustomOption = ({ submitting, submitSucceeded, disableInput, input, label, type, meta: { error, warning } }) => (
   <span className={`form-inline ${error ? 'has-error' : ''}`}>
     <label style="margin-right:10px;" className="control-label">{label}</label>
-    <input {...input} style="max-width:200px" className="form-control" type={type} disabled={disableInput || submitSucceeded || submitting}/>
+    <input {...input} style="max-width:184px" className="form-control" type={type} disabled={disableInput || submitSucceeded || submitting}/>
     {((error && <span className="control-label">{error}</span>) || (warning && <span>{warning}</span>))}
   </span>
 )
